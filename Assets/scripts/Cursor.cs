f746c6faaf54256dcc047aa99c54153ee53efffe
mikroDartVader <mike.korovin@gmail.com>
2017-10-28 00:12:18 +0300

    forgotten add-ons

---
+++ Assets/scripts/Cursor.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Castle castle; 
+    public Color tRed, tBlue, tGreen;
+    public Material instaceMaterial, cursorMaterial;
+    public Vector3 inCastlePos;
+
+    private bool onSurface;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (castle == null)
+            castle = GameObject.Find("CastleHandler").GetComponent<Castle>();
+        GetComponent<Renderer>().material = cursorMaterial;
+        GetComponent<BoxCollider>().enabled = false;
+        transform.tag = "Cursor";
+        setColor("blue");
+        gameObject.layer = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void setColor(string name)
+    {
+        if (name == "red")
+            GetComponent<Renderer>().material.color = tRed;
+        else
+        if (name == "blue")
+            GetComponent<Renderer>().material.color = tBlue;
+        else
+        if (name == "green")
+            GetComponent<Renderer>().material.color = tGreen;
+    }
+
+    void prepareToOperate(RaycastHit hit)
+    {
+        onSurface = true;
+        if (hit.transform.tag == "Buildable")
+        {
+            if (hit.normal.x > 0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos + new Vector3(1, 0, 0);
+            }
+            else
+            if (hit.normal.x < -0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos - new Vector3(1, 0, 0);
+            }
+            else
+            if (hit.normal.y > 0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos + new Vector3(0, 1, 0);
+            }
+            else
+            if (hit.normal.y < -0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos - new Vector3(0, 1, 0);
+            }
+            else
+            if (hit.normal.z > 0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos + new Vector3(0, 0, 1);
+            }
+            else
+            if (hit.normal.z < -0.999)
+            {
+                inCastlePos = hit.transform.GetComponent<Cursor>().inCastlePos - new Vector3(0, 0, 1);
+            }
+        }
+        else if (hit.transform.tag == "Grownd")
+        {
+            inCastlePos = castle.buildOnTheGrowndCoord(hit.point);
+        }
+        else onSurface = false;
+
+        if (onSurface)
+            transform.position = castle.getPosByElement(inCastlePos);
+    }
+
+    void operate(int button)
+    {
+        if (button == 0)
+        {
+            GameObject tmp = Instantiate(gameObject, transform.position, transform.rotation);
+            tmp.GetComponent<Cursor>().enabled = false;
+            tmp.tag = "Buildable";
+            tmp.GetComponent<Renderer>().material = instaceMaterial;
+            tmp.GetComponent<Cursor>().inCastlePos = inCastlePos;
+            tmp.layer = 0;
+        }
+    }
+
+    void hide()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    void show()
+    {
+        GetComponent<Renderer>().enabled = true;
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject Cursor;
+
+    public float maxBuildDist;
+
+    private Ray rayToLand;  //ray from camera to operate whith objects
+    private RaycastHit[] hits;
+
+    public GameObject handTool, block;
+    private GameObject operateObj;    //current player choice
+
+    private int actionMode;    //1->build , 2->operate with handed , 3->operate with landed
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rayToLand = new Ray();
+        actionMode = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rayToLand.origin = transform.position;
+        rayToLand.direction = transform.forward;
+        hits = Physics.RaycastAll(rayToLand, maxBuildDist).OrderBy(h => h.distance).ToArray();
+
+        if (Input.anyKey || Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            if (keyListener() == 2) //state == change object
+                changeObject(actionMode);
+        }
+
+        /*String objects = "";
+        foreach (RaycastHit hit in hits)
+            objects += hit.transform.name + "  ";
+        Debug.Log(objects + "  " + hits.Length); //printing all objects on the Ray and length of array of them*/
+
+        switch (actionMode)
+        {
+            case 1:
+                if (hits.Length > 0)
+                {
+                    operateObj.SendMessage("show");
+                    operateObj.SendMessage("prepareToOperate", hits[0]);
+                }
+                else
+                    operateObj.SendMessage("hide");
+                break;
+
+                /*case 2:
+                    operateObj.SendMessage("prepareToOperate", hits[0]);
+                    break;                 // maybe will use...*/
+
+                /*case 3:
+                    hits[0].transform.SendMessage("prepareToOperate", hits[0]);
+                    break;                 //will used when we create builded operateble objects */
+        }
+    }
+
+    int keyListener()  //return 0->no changes , 1->object operate , 2->change object , 3->change PhysPouse
+    {
+        byte a = 0, b = 0, c = 0;
+        if (Input.GetMouseButtonDown(0))
+        {
+            operateWithObj(0);
+            a = 1;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            operateWithObj(1);
+            a = 1;
+        }
+
+        int tmp = actionMode;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            actionMode = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            actionMode = 2;
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            actionMode = 3;
+        b = (byte)(tmp != actionMode? 1 : 0);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PhysPouse.on = !PhysPouse.on;
+            c = 1;
+        }
+        return a * 2 + b * 4 + c * 8;
+    }
+
+    private void changeObject(int mode)
+    {
+        Destroy(operateObj);
+        switch (mode)
+        {
+            case 1:
+                operateObj = Instantiate(block, hits[0].point, Quaternion.identity);
+                break;
+
+            case 2:
+                operateObj = Instantiate(handTool, transform);
+                operateObj.transform.localPosition = handTool.transform.position;
+                operateObj.transform.localRotation = handTool.transform.rotation;
+                break;
+
+            case 3:
+                break;
+        }
+    }
+
+    private void operateWithObj(int button)
+    {
+        if (actionMode < 3)
+            operateObj.SendMessage("operate", button);
+        /*else
+            hits[0].transform.SendMessage("operate"); //will used when we create builded operateble objects */
+    }
+}
